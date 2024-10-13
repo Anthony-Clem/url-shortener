16b37755398061d5f97db6fd2a62aad97edf6cb3
Anthony Clement <antclem6@gmail.com>
2024-10-13 10:20:42 -0400

    revert db.ts

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,28 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-// Create a new PrismaClient instance
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-// Extend the global object to hold the Prisma client instance
-declare global {
-  const prisma: PrismaClientSingleton | undefined;
-}
-
-// Initialize the Prisma client
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-// Export the Prisma client
 export default prisma;
 
-// Store the client instance in the global object in development
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
