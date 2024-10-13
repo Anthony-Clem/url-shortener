5bb42f86ec6816a4050916344665e62573fdfc73
Anthony Clement <antclem6@gmail.com>
2024-10-13 10:04:24 -0400

    prisma error testing

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,17 +1,28 @@
 import { PrismaClient } from "@prisma/client";
 
+// Create a new PrismaClient instance
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
+// Extend the global object to hold the Prisma client instance
+declare global {
+  var prisma: PrismaClientSingleton | undefined;
+}
+
+// Initialize the Prisma client
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
+// Export the Prisma client
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Store the client instance in the global object in development
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
